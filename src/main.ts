d58989da8069f43f4e66974e7989307494c52223
xyz-loops <marcelinus.fajar@student.com>
2023-10-31 10:42:47 +0700

    deploy2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { hostname } from 'os';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT, hostname:'0.0.0.0');
+  await app.listen(process.env.PORT);
 }
 bootstrap();
